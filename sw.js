4242c6cacdb77ae0ef7af521483eafb604cd3947
HungryBlueDev <hungrybluedev@gmail.com>
2020-07-06 01:22:21 +0530

    remove preload and change links

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 const files_to_cache = [
-  "fonts/PermanentMarker-Regular.ttf",
-  "images/favicon.ico",
-  "images/reset.png",
-  "images/info.png",
-  "images/logo.png",
-  "images/pvp.png",
-  "images/pvc.png",
-  "scripts/main.js",
-  "styles/fonts.css",
-  "styles/style.css",
-  "index.html",
-  "/",
+  "https://hungrybluedev.github.io/hbd-tictactoe/fonts/PermanentMarker-Regular.ttf",
+  "https://hungrybluedev.github.io/hbd-tictactoe/images/favicon.ico",
+  "https://hungrybluedev.github.io/hbd-tictactoe/images/reset.png",
+  "https://hungrybluedev.github.io/hbd-tictactoe/images/info.png",
+  "https://hungrybluedev.github.io/hbd-tictactoe/images/logo.png",
+  "https://hungrybluedev.github.io/hbd-tictactoe/images/pvp.png",
+  "https://hungrybluedev.github.io/hbd-tictactoe/images/pvc.png",
+  "https://hungrybluedev.github.io/hbd-tictactoe/scripts/main.js",
+  "https://hungrybluedev.github.io/hbd-tictactoe/styles/fonts.css",
+  "https://hungrybluedev.github.io/hbd-tictactoe/styles/style.css",
+  "https://hungrybluedev.github.io/hbd-tictactoe/index.html",
+  "https://hungrybluedev.github.io/hbd-tictactoe/",
 ];
 
 const cache_name = "HBDTicTacToe-v1";
